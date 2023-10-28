ea95ed155673008479c84233df9c203f43161de6
Iven Winkelmann <winkelmann.iven@gmail.com>
2023-10-28 16:37:23 +0200

    added aliases

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -68,6 +68,7 @@
     gh
     azure-cli
     terraform
+    terramate
     fd
     fzf
     delta
--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -2,13 +2,20 @@
   programs.zsh = {
     enable = true;
     shellAliases = {
+      # nixos
       rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config";
       update = "home-manager switch --flake ~/nixos-config";
       cleanup = "sudo nix-store --gc && nix-env --delete-generations +1";
-      nd = "mkdir -pv";
-      nf = "touch";
-      deldir = "rm -rIv";
+
+      # config
+      config-nix = "cd ~/nixos-config && nvim";
       config-nvim = "cd ~/nixos-config/home-manager/nvim && nvim";
+
+      # dev shells
+      dev-rust = "nix develop ~/nixos-config#rust -c $SHELL";
+      dev-py11 = "nix develop ~/nixos-config#python11 -c $SHELL";
+      dev-go = "nix develop ~/nixos-config#go -c $SHELL";
+      dev-zig = "nix develop ~/nixos-config#zig -c $SHELL";
     };
     oh-my-zsh = {
       enable = true;
