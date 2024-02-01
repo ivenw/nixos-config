0a96f58953d931f9dabbfbb3244047b3e182fd50
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-02-01 14:31:59 +0100

    updates

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -7,16 +7,19 @@
         variant = "mocha";
         accents = ["blue"];
         size = "compact";
-        # tweaks = ["rimless" "black"];
+        tweaks = [
+          "rimless"
+          # "black"
+        ];
       };
     };
     cursorTheme = {
       name = "Catppuccin-Mocha-Dark-Cursors";
       package = pkgs.catppuccin-cursors.mochaDark;
     };
-    font = {
-      name = "JetBrainsMono Nerd Font";
-    };
+    # font = {
+    #   name = "JetBrainsMono Nerd Font";
+    # };
     gtk3.extraConfig = {
       Settings = ''
         gtk-application-prefer-dark-theme=1
@@ -34,6 +37,7 @@
       color-scheme = "prefer-dark";
       enable-hot-corners = true;
       clock-show-weekday = true;
+      enable-animations = true;
     };
     "org/gnome/desktop/calendar" = {
       show-weekdate = true;
@@ -43,6 +47,20 @@
     };
     "org/gnome/system/location" = {
       enabled = true;
+    };
+    "org/gnome/shell/enable-extensions" = [
+      "just-perfection-desktop@just-perfection"
+    ];
+    "org/gnome/shell/extensions/just-perfection" = {
+      theme = true;
+      dash = false;
+      workspace = false;
+      overlay-key = false;
+      top-panel-position = 1;
+      clock-menu-position = 1;
+      clock-menu-position-offset = 6;
+      animation = 5;
+      notification-banner-position = 5;
     };
   };
 }
