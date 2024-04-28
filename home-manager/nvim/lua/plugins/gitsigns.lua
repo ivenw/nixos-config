412e1e0b2ee4dbe2580b66b449ae75e09162d091
Iven Winkelmann <winkelmann.iven@gmail.com>
2024-04-28 18:43:27 +0200

    added blame toggle

--- home-manager/nvim/lua/plugins/gitsigns.lua
+++ home-manager/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,11 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	config = function()
-		require("gitsigns").setup()
+		local gitsigns = require("gitsigns")
+		gitsigns.setup()
+
+		local km = vim.keymap
+
+		km.set("n", "<leader>cb", gitsigns.toggle_current_line_blame, { desc = "Toggle blame" })
 	end,
 }
